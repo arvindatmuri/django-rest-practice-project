06264987df4d28ef635fc5ec81ddb80b2e00455c
Arvind Atmuri <arvindatmuri@gmail.com>
2023-04-11 23:18:34 +1000

    Create Posts, Get all posts and get post by id

--- drfadmin/blogs/urls.py
+++ drfadmin/blogs/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import CreatePostView
+from .views import CreatePostView, ListPostsView, PostDetailView
 
 urlpatterns = [
     path('posts/', CreatePostView.as_view(), name='create_post'),
+    path('posts/list/', ListPostsView.as_view(), name='list_posts'),
+    path('posts/<int:id>/', PostDetailView.as_view(), name='post_detail'),
 ]
--- drfadmin/blogs/views.py
+++ drfadmin/blogs/views.py
@@ -7,4 +7,15 @@ from .serializers import PostSerializer
 class CreatePostView(generics.CreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    print(PostSerializer.data)
+    # print(PostSerializer.data)
+
+
+class ListPostsView(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetailView(generics.RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    lookup_field = 'id'
